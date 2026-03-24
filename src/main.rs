08734e73ef7a647b0cd39b58c6e338c7f751df50
mathrews <mateusps4gg@gmail.com>
2026-03-24 14:40:46 -0300

    Done: connection with DB, .env

--- src/main.rs
+++ src/main.rs
@@ -1,9 +1,30 @@
-use tokio_postgres::{NoTls, Error};
+use dotenv::{dotenv, var};
+use tokio_postgres::{Error, NoTls};
 
 #[tokio::main]
 async fn main() -> Result<(), Error> {
+    // Setup environment variables
+    dotenv().ok();
+    /*
+    let postgres_db: String = match var("POSTGRES_DB") {
+        Ok(v) => v,
+        Err(e) => panic!("Erro {e:?}")
+    };
+    */
+    let postgres_db: String = var("POSTGRES_DB").ok().unwrap();
+    let postgres_user: String = var("POSTGRES_USER").ok().unwrap();
+    let postgres_password: String = var("POSTGRES_PASSWORD").ok().unwrap();
+
     // Connection string format: host=localhost user=username password=password dbname=database
-    let (client, connection) = tokio_postgres::connect("host=localhost user=postgres dbname=sebo_project password=1234", NoTls).await?;
+    let (client, connection) = tokio_postgres::connect(
+        format!(
+            "host=localhost user={} dbname={} password={}",
+            postgres_user, postgres_db, postgres_password
+        )
+        .as_str(),
+        NoTls,
+    )
+    .await?;
 
     // Spawn the connection to run independently
     tokio::spawn(async move {
@@ -13,8 +34,29 @@ async fn main() -> Result<(), Error> {
     });
 
     // Now you can execute queries using the client
-    let rows = client.query("SELECT * FROM users", &[]).await?;
-    println!("Connected and queried: {:?}", rows);
+    // client
+    //     .query(
+    //         "INSERT INTO users (name, email, login, password, cell_number, role, created_at, updated_at)
+    //         VALUES
+    //         ('João Silva', 'joao.silva@email.com', 'joaosilva', 'senha123', '85999990001', 1, NOW(), NOW()),
+    //         ('Maria Oliveira', 'maria.oliveira@email.com', 'mariaoliveira', 'senha123', '85999990002', 2, NOW(), NOW()),
+    //         ('Carlos Souza', 'carlos.souza@email.com', 'carlossouza', 'senha123', '85999990003', 1, NOW(), NOW()),
+    //         ('Ana Santos', 'ana.santos@email.com', 'anasantos', 'senha123', '85999990004', 3, NOW(), NOW()),
+    //         ('Pedro Lima', 'pedro.lima@email.com', 'pedrolima', 'senha123', '85999990005', 2, NOW(), NOW());",
+    //         &[],
+    //     )
+    //     .await?;
+
+    let rows = client.query("SELECT * FROM users;", &[]).await?;
+
+    // Iterate over results
+    for row in rows {
+        let id: i32 = row.get("id");
+        let name: String = row.get("name");
+        let email: String = row.get("email");
+
+        println!("{} | {} | {}", id, name, email);
+    }
 
     Ok(())
 }
