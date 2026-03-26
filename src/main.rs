65b94188db901fdd89f69cee81b1e473a2f990e6
mathrews <mateusps4gg@gmail.com>
2026-03-26 15:21:11 -0300

    learning modules in routes

--- src/main.rs
+++ src/main.rs
@@ -5,6 +5,12 @@ use axum::{
     Router,
 };
 
+//mod routes {
+//    pub mod user_routes;
+//}
+
+mod routes;
+
 #[tokio::main]
 async fn main() -> Result<(), Error> {
     // Setup environment variables
@@ -71,7 +77,11 @@ async fn main() -> Result<(), Error> {
     }
 
     // build our application with a single route
-    let app = Router::new().route("/", get(|| async { "Hello, World!" }));
+
+    println!("{} {}", routes::user_routes::hello(), routes::hello());
+
+    let app = Router::new()
+        .route("/", get(|| async { "Hello, World!" }));
 
     // run our app with hyper, listening globally on port 3000
     let listener = tokio::net::TcpListener::bind("0.0.0.0:3000").await.unwrap();
